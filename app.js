1971f3b1000b613220ebf2524a257cfe3c81f4a9
Vadim <vadim.putrov@gmail.com>
2023-02-10 20:19:36 +0200

    add simple static server on express and add index.html

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const port = 9000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
